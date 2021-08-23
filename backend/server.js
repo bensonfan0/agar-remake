fe0ed8992fae9c1462e58ba6bfde19c4d89b1b38
bensonfan0 <benson.fan0@gmail.com>
2021-08-22 21:30:40 -0700

    socket.io added
    can see unique players interacting with game.
    
    time to move logic to backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 import express from 'express';
+import { Server } from 'socket.io';
+import http from 'http';
+
+
 
 const app = express(); 
+
 const port = process.env.PORT || 3000; 
 
 app.use(express.static('./frontend/build'));
 
+// Setup socket.io
+const server = http.createServer(app);
+const io = new Server(server);
 
 let incrementMe = 0;
 setInterval(() => {
@@ -13,12 +21,18 @@ setInterval(() => {
   });
   incrementMe++;
 }, 500);
-  
-app.listen(port, () => {
-  console.log(`Server listening on ${port}`);
-});
 
 
+io.on('connection', (socket) => {
+  console.log('SOCKET.IO working -> a user connected with id: ' + socket.id);
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+})
+
+server.listen(port, () => {
+  console.log(`Server listening on ${port}`)
+})
 
 // Setup the Game
 const game = {};
